c924702763ae7e31c022e009488c0ef3ac882372
kelley·胡 <40583480+cappuccino5@users.noreply.github.com>
2019-05-30 11:50:02 +0800

    Update formdata_file.go

--- formdata_file.go
+++ formdata_file.go
@@ -1,45 +1,48 @@
-package controller
+package fastdfs
 
 import (
 	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"go-hisens/commons"
 	"image"
 	"image/jpeg"
 	"image/png"
-	"io"
 	"io/ioutil"
 	"mime/multipart"
 	"net/http"
-	"net/url"
 	"path"
 )
 
 type FileServer struct {
-	Url      string
-	Scene    string
-	FromName string
+	Url   string
+	Scene string
 }
 
-func initServer(rootPath, fromName string) *FileServer {
+var con = commons.AppConfig
+
+func initServer() *FileServer {
 	return &FileServer{
-		Url:      "http://10.0.0.86:8083/group1/upload", 
-		Scene:    rootPath,
-		FromName: fromName,
+		Url:   con.FileServer + con.GroupName + "/upload",
+		Scene: con.Scene,
 	}
+}
+
+var fastdfs *FileServer
+
+func init() {
+	fastdfs = initServer()
 }
 
 //*web或者app上传文件到主服务器，主服务器将文件转存到fastdfs文件存储服务器,本done是本地测试环境*//
-// rootPath = 保存的路径名字 fromName = 保存的文件名
-func ImagesTest(rootPath, fromName string, f *multipart.FileHeader) (map[string]interface{}, error) {
-	if rootPath == "" || fromName == "" {
+// rootPath = 保存的路径名字 fromName = 表单key
+func UploadFile(FileDir string, f *multipart.FileHeader) (map[string]interface{}, error) {
+	if FileDir == "" {
 		return nil, errors.New("param is empty")
 	}
-	ser := initServer(rootPath, fromName)
-	imageUrl, err := ser.FileUpdateRequest(f)
+	imageUrl, err := fastdfs.fastdfsRequest(f, FileDir)
 	if err != nil {
-		fmt.Println("save image err:", err)
 		return nil, err
 	}
 	resMap := make(map[string]interface{}, 0)
@@ -49,7 +52,6 @@ func ImagesTest(rootPath, fromName string, f *multipart.FileHeader) (map[string]
 		resMap["url"] = imageUrl + "?download=0"
 		resMap["thumb_url"] = fmt.Sprintf("%s?download=0&width=%v&height=%v", imageUrl, width, height)
 	}
-	fmt.Println(resMap)
 	return resMap, nil
 }
 
@@ -73,37 +75,40 @@ func tailorImage(header *multipart.FileHeader) (int, int) {
 }
 
 // 文件存储至fastdfs文件服务器,返回url
-func (ser *FileServer) FileUpdateRequest(header *multipart.FileHeader) (string, error) {
+func (ser *FileServer) fastdfsRequest(header *multipart.FileHeader, showDir string) (string, error) {
 	var resUrl string
 	file, err := header.Open()
 	if err != nil {
 		return resUrl, err
 	}
-	buf := new(bytes.Buffer)
-	w := multipart.NewWriter(buf)
-	// 自定义路径名字
-	err = w.WriteField("scene", ser.Scene)
-	if err != nil {
-		return resUrl, err
-	}
-	fw, err := w.CreateFormFile(ser.FromName, header.Filename)
-	if err != nil {
-		return resUrl, err
-	}
-	_, err = io.Copy(fw, file)
-	if err != nil {
-		return resUrl, err
-	}
+	defer file.Close()
+	tempbuf := make([]byte, header.Size+1)
+	n, _ := file.Read(tempbuf)
+	tempbuf = tempbuf[:n]
+	filebuf := new(bytes.Buffer)
+	w := multipart.NewWriter(filebuf)
+	w.WriteField("scene", ser.Scene)
+	w.WriteField("file_dir", showDir)
+	w.WriteField("output", "json")
+	fw, err := w.CreateFormFile("file", header.Filename)
+	fw.Write(tempbuf)
 	methods := w.FormDataContentType()
 	w.Close()
-	res, err := HttpPostMux(ser.Url, buf, methods)
+	res, err := HttpPostMux(ser.Url, filebuf, methods)
 	if err != nil {
 		return resUrl, err
 	}
-	resUrl = fmt.Sprintf("%s", res)
+	fileInfo := make(map[string]interface{}, 0)
+	err = json.Unmarshal(res, &fileInfo)
+	if err != nil {
+		return resUrl, err
+	}
+	if size, ok := fileInfo["size"].(float64); !ok || size != float64(header.Size) {
+		return resUrl, errors.New("file size error")
+	}
+	resUrl, _ = fileInfo["url"].(string)
 	return resUrl, nil
 }
-
 
 // 重新编辑图片大小,获取缩略图大小
 func resizeImage(img image.Image) (int, int) {
@@ -122,13 +127,12 @@ func resizeImage(img image.Image) (int, int) {
 
 // http post 表单请求
 func HttpPostMux(posturl string, buf *bytes.Buffer, mod string) ([]byte, error) {
-	fmt.Println("req:", posturl)
+	fmt.Println("fastdfs req:", posturl)
 	req, err := http.NewRequest("POST", posturl, buf)
 	if err != nil {
 		return nil, err
 	}
 	req.Header.Set("Content-Type", mod)
-
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -139,71 +143,5 @@ func HttpPostMux(posturl string, buf *bytes.Buffer, mod string) ([]byte, error) 
 	if err != nil {
 		return nil, err
 	}
-	return body, nil
-}
-
-//Post 请求方法
-func HttpPostProxy(reqUrl string, params map[string]interface{}) ([]byte, error) {
-	proxy := func(_ *http.Request) (*url.URL, error) {
-		return url.Parse("http://127.0.0.1:1080")
-	}
-	transport := &http.Transport{Proxy: proxy}
-	c := &http.Client{Transport: transport}
-	query := url.Values{}
-	for key, value := range params {
-		query.Add(key, fmt.Sprintf("%v", value))
-	}
-	resp, err := c.PostForm(reqUrl, query)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-	return body, nil
-}
-
-func HttpGet(getUrl string) ([]byte, error) {
-	fmt.Println(getUrl)
-	resp, err := http.Get(getUrl)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	return body, nil
-}
-
-func HttpPostJson(url string, data interface{}) ([]byte, error) {
-	buf, err := json.Marshal(data)
-	if err != nil {
-		return nil, err
-	}
-
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(buf))
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Set("Content-Type", "application/json")
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
 	return body, nil
 }
